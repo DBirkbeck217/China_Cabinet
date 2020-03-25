21c2aab81bbf060c8dbb67da42719bafab8a66d4
Dan Birkbeck <DBirkbeck217@gmail.com>
2020-03-25 16:31:41 -0400

    updated game conditions

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-let mode = "src"
+let mode = "src";
 
-let tower1 = document.getElementById("tower1")
-tower1.addEventListener("click", moveDisk)
-let tower2 = document.getElementById("tower2")
-tower2.addEventListener("click", moveDisk)
-let tower3 = document.getElementById("tower3")
-tower3.addEventListener("click", moveDisk)
+let tower1 = document.getElementById("tower1");
+tower1.addEventListener("click", moveDisk);
+let tower2 = document.getElementById("tower2");
+tower2.addEventListener("click", moveDisk);
+let tower3 = document.getElementById("tower3");
+tower3.addEventListener("click", moveDisk);
 
-let currentDisk
-let diskVal
+let currentDisk;
+let diskVal;
 
 function moveDisk(event) {
-    console.log(mode)
-    let currentTower = event.currentTarget
+  let currentTower = event.currentTarget;
 
-    if (mode == "src") {
-        currentDisk = currentTower.lastElementChild
-        diskVal = currentDisk.dataset.size
-        mode = "dest"
+  if (mode == "src") {
+    currentDisk = currentTower.lastElementChild;
+    diskVal = currentDisk.dataset.size;
+    currentDisk.style.border = "thick solid #0000FF";
+
+    mode = "dest";
+  } else if (mode == "dest") {
+    currentDisk.style.border = "none";
+
+    if (currentTower.lastElementChild === null) {
+      currentTower.appendChild(currentDisk);
     }
 
-    else if (mode == "dest") {
-        if (currentTower.lastElementChild)
-        console.log(diskVal,currentTower.lastElementChild.dataset.size)
-
-        if (currentTower.lastElementChild && diskVal < currentTower.lastElementChild.dataset.size) {
-            return alert("No disk may be placed on top of a smaller disk! Try again")
-        } 
-        else {
-            currentTower.appendChild(currentDisk)
-            mode = "src"
-        }
+    if (
+      diskVal > currentTower.lastElementChild.dataset.size &&
+      currentTower.lastElementChild
+    ) {
+      alert("No disk may be placed on top of a smaller disk! Try again");
+      mode = "src";
+    } else {
+      currentTower.appendChild(currentDisk);
+      mode = "src";
     }
+  }
 
-    if (tower3.childElementCount > 3) {
-        return alert("You Win!")
-    }
-
+  if (tower1.childElementCount === 1) {
+    return alert("You Win!");
+  }
 }
